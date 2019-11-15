57770c8dd872f1017e7b1bc66f10010bd9643317
Caijinglong <cjl_spy@163.com>
2019-11-15 15:50:02 +0800

    Update hello world.

--- hello-world.go
+++ hello-world.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("hello world")
+
+	_ = gin.New()
+	fmt.Println("gin create success")
 }
